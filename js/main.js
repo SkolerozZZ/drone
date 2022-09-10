68c5e71b63ea345bccd15ab242a336acfa014f6a
SkolerozZZ <45293977+SkolerozZZ@users.noreply.github.com>
2022-09-10 14:25:24 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-
+$(function(){
+    $('.slider').slick({
+        arrows: false,
+        fade: true,
+        autoplay: 3000,
+    });
+});
 
 $(function(){
-    $('a[href^="#"]').click(function(){
-    let target = $(this).attr('href');
-    $('html, body').animate({scrollTop: $(target).offset().top}, 1500);//800 - длительность скроллинга в мс
-    return false;
+    $('.slick__slider').slick({
+        arrows: false,
+        fade: true,
+        autoplay: 3000,
     });
-    });
-
-
+});
