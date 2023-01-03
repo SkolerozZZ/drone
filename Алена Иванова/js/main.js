98ac7f90bd143eccfb4f83330c151794a5f51aa6
SkolerozZZ <45293977+SkolerozZZ@users.noreply.github.com>
2023-01-04 02:06:35 +0300

    Add files via upload

---
+++ Алена Иванова/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('.slider').slick({
+        arrows: false,
+        fade: true,
+        autoplay: 3000,
+    });
+});
+
+$(function(){
+    $('.slick__slider').slick({
+        arrows: false,
+        fade: true,
+        autoplay: 3000,
+    });
+});
